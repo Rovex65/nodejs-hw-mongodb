90ef93f76d7d848e0e01a34a2648e351dc5be876
Rovex65 <rovex65@gmail.com>
2024-10-25 04:04:37 +0200

    add validation, pagination, sorting

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,8 @@
+const parseType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+  const isContactType = (contactType) =>
+    ['work', 'home', 'personal'].includes(contactType);
+
+  if (isContactType(contactType)) return contactType;
+};
